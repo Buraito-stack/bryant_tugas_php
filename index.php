f1175a08ad3dadd2c6d6ef4c3d616774e26d85f0
Buraito-stack <bryantedyluke@gmail.com>
2024-07-19 14:13:56 +0700

    Hello Hello my name is Bryant, I am a programmer. I am learning at timedoors.

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 echo "Hello";
 echo "Hello my name is Bryant";
 echo "Hello my name is Bryant, I am a programmer";
+echo "Hello Hello my name is Bryant, I am a programmer. I am learning at timedoors.";
 ?>
